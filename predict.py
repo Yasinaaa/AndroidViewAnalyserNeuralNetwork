2861ed40939ad5c7774d43097a99e0a45cbe1572
Zalyalova Yasina <zalyalova@infomatika.ru>
2018-06-26 15:01:23 +0300

    Try to make neural network for buttons, fab, checkbox

--- predict.py
+++ predict.py
@@ -2,11 +2,11 @@ import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
+test_image = image.load_img('dataset/single_prediction/test1.PNG', target_size = (64, 64))
 
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
-classifier = load_model("cats_dogs.h5")
+classifier = load_model("android.h5")
 result = classifier.predict(test_image)
 print(result[0][0])
 if result[0][0] == 1.0:
