0183968ee2fb2d48128e8169fdacb31526fdcaad
Zalyalova Yasina <zalyalova@infomatika.ru>
2018-06-29 14:39:25 +0300

    New neural network

--- predict.py
+++ predict.py
@@ -2,12 +2,12 @@ import numpy as np
 from keras.preprocessing import image
 from keras.models import load_model
 
-test_image = image.load_img('dataset/single_prediction/test2.PNG', target_size = (24, 24))
+test_image = image.load_img('dataset/single_prediction/test4.PNG', target_size = (24, 24))
 
 test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 classifier = load_model("android.h5")
-result = classifier.predict(test_image)
+result = classifier.predict_proba(test_image)
 print(result[0][0])
 if result[0][0] == 1.0:
     prediction = 'dog'
